ac2af7771fd826a3fb913c2e799f3b975960778d
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-12-24 18:12:09 +0400

    Edit regular expressions

--- lab_3/consts.py
+++ lab_3/consts.py
@@ -6,17 +6,18 @@ DEFAULT_FILE_PATH = "79.csv"
 FILE_ENCODING = "utf-16"
 CSV_DELIMITER = ";"
 
-# -------- REGEX PATTERNS -------- #
-EMAIL_PATTERN = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-HEIGHT_PATTERN = r'^[12]\.\d{1,5}$'
-INN_PATTERN = r'^\d{12}$'
-PASSPORT_PATTERN = r'^\d{2}\s\d{2}\s\d{6}$'
-OCCUPATION_PATTERN = r'^[А-Яа-яA-Za-z\s\-\./,\(\)]+$'
-LATITUDE_PATTERN = r'^-?\d{1,2}\.\d+$'
-HEX_COLOR_PATTERN = r'^#[0-9A-Fa-f]{6}$'
-ISSN_PATTERN = r'^\d{4}-\d{4}$'
-UUID_PATTERN = r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$'
-TIME_PATTERN = r'^\d{2}:\d{2}:\d{2}(\.\d+)?$'
+
+# -------- REGEX PATTERNS (замени своими!) -------- #
+EMAIL_PATTERN = r".+"
+HEIGHT_PATTERN = r".+"
+INN_PATTERN = r".+"
+PASSPORT_PATTERN = r".+"
+OCCUPATION_PATTERN = r".+"
+LATITUDE_PATTERN = r".+"
+HEX_COLOR_PATTERN = r".+"
+ISSN_PATTERN = r".+"
+UUID_PATTERN = r".+"
+TIME_PATTERN = r".+"
 
 PATTERNS = [
     EMAIL_PATTERN,
